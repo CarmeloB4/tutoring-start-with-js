b263ed4d3f66f7a67b9bdf9072c579e2fb9f45a9
Carmelo Bastillo <66789080+CarmeloB4@users.noreply.github.com>
2022-03-28 14:51:06 +0200

    Create file for object exercise

---
+++ object.js
@@ -0,0 +1,14 @@
+// 1.Stampa tutti le chiavi di questo oggetto
+// 2.Stampa tutti i valori di questo oggetto
+
+const dog = {
+    name: "Pluto",
+    age: 2,
+    color: "black",
+    isQuiet: true
+}
+
+console.log(Object.keys(dog))
+console.log(Object.values(dog))
+
+// In questo esempio sugli oggetti, abbiamo visto solamente questi due semplici metodi, parliamo in modo più approfondito degli oggetti nel file readme
